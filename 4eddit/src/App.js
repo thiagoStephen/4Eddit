46b66a461d1168909287c70867d544a7f37c7cee
thiagoStephen <thiagosop@yahoo.com>
2021-06-01 06:19:26 -0300

    iniciando o projeto. configurações iniciais e modelagem da tela signUp

---
+++ 4eddit/src/App.js
@@ -0,0 +1,12 @@
+import SignUpComponent from './Components/SignUp/SignUp'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUpComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 4eddit/src/Components/SignUp/SignUp.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { ContainerForm, ContainerSignUp } from "./signUp_styles";
+
+
+export default function SignUpComponent () {
+   
+    
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        onchange( name, value)
+    }
+
+    return (
+        
+        <ContainerSignUp>
+            <ContainerForm>
+                <h2>Cadastrar</h2>
+                <input 
+                    placeholder="Nome de usuário"
+                    value={FormData.userName}
+                    name="userName"
+                    type="text"
+                    required
+                    onChange={handleInputChange}>
+                </input>
+                <input 
+                    placeholder="Email"
+                    value={FormData.userName}
+                    name="email"
+                    type="text"
+                    required
+                    onChange={handleInputChange}>
+                </input>
+                <input 
+                    placeholder="senha"
+                    value={FormData.userName}
+                    name="password"
+                    type="password"
+                    required
+                    onChange={handleInputChange}>
+                </input>
+            </ContainerForm>
+        </ContainerSignUp>
+    )
+}
